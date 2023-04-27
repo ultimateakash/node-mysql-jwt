2bd618798b504dd51b1307767616cf9a9648f957
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2023-04-27 09:48:09 +0530

    Update cache util

--- utils/cache.util.js
+++ utils/cache.util.js
@@ -1,8 +1,10 @@
 const Keyv = require('keyv'); 
 const keyv = new Keyv();
 
-exports.set = (key, value, ttl = 0) => keyv.set(key, value, ttl);
+exports.set = (key, value, ttl = undefined) => keyv.set(key, value, ttl);
 
 exports.get = (key) => keyv.get(key); 
 
 exports.del = (key) => keyv.delete(key);
+
+exports.clear = () => keyv.clear();
